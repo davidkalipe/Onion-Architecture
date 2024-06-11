a49456363dfd3648b6f48dd066451e6436d5e143
jackboy14 <kalipedavid@gmail.com>
2024-06-11 15:31:16 +0000

    _

--- Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ecd2f3dc75dcd6e950b486b7635a8441d05feb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8682403c41cd26bcdf24a1d5652c9d10091af1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
